cecc1ef2f244c55ccc74538f6a9b005dc484cb7e
hsethia21 <sethia.h@husky.neu.edu>
2020-01-28 22:24:33 -0500

    Updated rendering

--- src/main/webapp/admin/users.js
+++ src/main/webapp/admin/users.js
@@ -23,6 +23,7 @@
         userService.deleteUser(userId)
             .then(response => {
                 users.splice(index, 1)
+                findAllUsers()
                 renderUsers()
             })
     }
@@ -60,6 +61,7 @@
                 $firstNameFld.val(actualUser.firstName)
                 $lastNameFld.val(actualUser.lastName)
                 $roleFld.val(actualUser.role)
+
             })
     }
 
@@ -87,22 +89,6 @@
         for(let u in users) {
             let user = users[u]
 
-            /*<tbody class="wbdv-tbody">
-                    <tr class="wbdv-template wbdv-user wbdv-hidden">
-                        <td class="wbdv-username">ada</td>
-                        <td>&nbsp;</td>
-                        <td class="wbdv-first-name">Ada</td>
-                        <td class="wbdv-last-name">Lovelace</td>
-                        <td class="wbdv-role">FACULTY</td>
-                        <td class="wbdv-actions">
-                    <span class="float-right">
-                      <i id="wbdv-remove" class="fa-2x fa fa-times wbdv-remove"></i>
-                      <i id="wbdv-edit" class="fa-2x fa fa-pencil wbdv-edit"></i>
-                    </span>
-                        </td>
-                    </tr>
-                    </tbody>*/
-
             $body = $(`<tbody class="wbdv-tbody">`)
             $row = $(`<tr class="wbdv-user wbdv-hidden">`)
             $td= $(`<td>`)
@@ -112,10 +98,6 @@
 
             $editBtn = $(`<i id="wbdv-edit" class="fa fa-pencil wbdv-edit"></i>`)
             $editBtn.click(() => editUser(u))
-
-            /*$span = $(`<span class="float-right"> </span>`)
-            $span.html($deleteBtn)
-            $span.html($editBtn)*/
 
             $col1 = $td.html(user.username)
             $td = $(`<td>`)
@@ -134,39 +116,6 @@
             ($col4).append($col5).append($col6)
             $renderBody = $body.html($renderRow)
 
-
-            /*$body.append($row)
-            $body.append($td).append(user.username)
-            $body.append($td)
-            $body.append(`&nbsp`)
-            $body.append($td)
-            $body.append(user.firstName)
-            $body.append($td)
-            $body.append(user.lastName)*/
-
-
-            /*$li = $("<tr></tr>")
-            $td = $("<td></td>")
-            $tdc = $("</td>")
-
-            $td.html(user.username)
-
-            $li.html($td)
-
-//            $li.append($tdc)
-
-            $li.html($span)
-
-            $li.append($td)
-                        $li.append($deleteBtn)
-                        $li.append($tdc)
-
-                        $li.append($td)
-                                    $li.append($editBtn)
-                                    $li.append($tdc)*/
-
-
-
             $adminTable.append($renderRow)
         }
     }
@@ -178,6 +127,11 @@
                 renderUsers()
             })
     }
+
+    function searchUser(){
+
+    }
+
     function main() {
         $userList = $("#userList")
         $adminTable = $("#adminTable")
@@ -193,6 +147,9 @@
 
         $updateBtn = $(".wbdv-update")
         $updateBtn.click(updateUser)
+
+        $searchBtn = $(".wbdv-update")
+        $searchBtn.click(searchUser)
 
         userService
             .findAllUsers()
